ea21deb060aefcf9d3f0cbb3185997ef6f8de7e4
DmitriX <dodge.241294@gmail.com>
2025-07-26 12:21:21 +0300

    Display MainWindow

--- main.cpp
+++ main.cpp
@@ -4,8 +4,21 @@
 
 #include <windows.h>
 
+#include "./window/MainWindow/MainWindow.hpp"
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    
+    MainWindow main_window(L"Kulichik Game", 0, 0, 1540, 825);
+
+    if (!main_window.create(WS_OVERLAPPEDWINDOW)) return 0;
+
+    ShowWindow(main_window.getWindow(), nCmdShow);
+
+    MSG msg = {};
+
+    while(GetMessage(&msg, NULL, 0 , 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
     return 0;
 }
